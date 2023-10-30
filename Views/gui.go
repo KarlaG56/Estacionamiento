86b6f03f5bee9af9d95617a0fc539b30228eb860
Keyel <keyel@localhost.localdomain>
2023-10-30 08:55:27 -0600

    [Update]

--- Views/gui.go
+++ Views/gui.go
@@ -10,6 +10,7 @@ import (
 	"image/color"
 	"math/rand"
 	"time"
+	"fmt"
 )
 
 const (
@@ -34,14 +35,16 @@ func ShowGUI() {
 
 	window.Show()
 
-	// Comenzar a agregar autos al estacionamiento
+	// Asegurar el tamaño y ubicación de los autos
 	go runSimulation(window)
 }
 
 func runSimulation(window fyne.Window) {
 	for {
 		if len(carGraphics) < maxCars {
-			AddCarToGarage()
+			fyne.CurrentApp().Send(func() {
+				AddCarToGarage()
+			})
 		}
 		UpdateGarageView(window)
 		time.Sleep(2 * time.Second)
@@ -49,19 +52,35 @@ func runSimulation(window fyne.Window) {
 }
 
 func AddCarToGarage() {
-	car := canvas.NewRectangle(color.RGBA{R: uint8(rand.Intn(255)), G: uint8(rand.Intn(255)), B: uint8(rand.Intn(255)), A: 255})
-	car.Resize(fyne.NewSize(20, 20))
-	car.Move(fyne.NewPos(float32(rand.Intn(garageWidth-20)), float32(rand.Intn(garageHeight-20))))
+	car := canvas.NewRectangle(color.RGBA{
+		R: uint8(rand.Intn(255)),
+		G: uint8(rand.Intn(255)),
+		B: uint8(rand.Intn(255)),
+		A: 255,
+	})
+	car.Resize(fyne.NewSize(20, 20)) // Asegurar el tamaño visible
+	car.Move(fyne.NewPos(float32(rand.Intn(garageWidth-20)), float32(rand.Intn(garageHeight-20))) // Asegurar la posición visible
 	carGraphics = append(carGraphics, car)
+
+	// Mensajes de registro para depurar
+	fmt.Println("Auto agregado al estacionamiento.")
 }
 
 func UpdateGarageView(window fyne.Window) {
-	contentObjects := []fyne.CanvasObject{canvas.NewRectangle(theme.BackgroundColor())}
+	contentObjects := make([]fyne.CanvasObject, 0) // Crear una nueva lista para evitar duplicaciones
+	// Agregar el rectángulo del estacionamiento a la lista de objetos a mostrar
+	contentObjects = append(contentObjects, canvas.NewRectangle(theme.BackgroundColor()))
 
 	for _, car := range carGraphics {
+		// Agregar cada auto a la lista de objetos a mostrar
 		contentObjects = append(contentObjects, car)
 	}
 
-	window.SetContent(container.NewMax(contentObjects...))
-	window.Canvas().Refresh(contentObjects[0])
+	// Actualizar la ventana con los objetos
+	content := container.NewMax(contentObjects...)
+	window.SetContent(content)
+	window.Canvas().Refresh(content)
+
+	// Mensajes de registro para depurar
+	fmt.Println("Actualización de la vista.")
 }
